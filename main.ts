4ea03f02c750c124563841a70f862dcdff859dff
Peter Teichman <peter@teichman.org>
2021-10-23 16:31:35 -0700

    First implementation & release of some additional Emacs keys

--- main.ts
+++ main.ts
@@ -1,135 +1,92 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, MarkdownView, Plugin } from 'obsidian';
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class MoreEmacsPlugin extends Plugin {
+	// Some notes for future me:
+	//
+	// If having trouble getting a hotkey to activate, try binding the
+	// command to the desired key in Obsidian's settings; you can then
+	// check .obsidian/hotkeys.json to see how to configure that key.
+	//
+	// Editor is an interface that spans CodeMirror versions, but doesn't
+	// provide much. We jump through some hoops to get the active editor
+	// in our callbacks below.
+	//
+	// CodeMirror commands: https://codemirror.net/doc/manual.html#commands
 
 	async onload() {
-		await this.loadSettings();
+		if (!window.CodeMirror || window.CodeMirror.version[0] > "5") {
+			console.log("MoreEmacsPlugin not updated for new CodeMirror " + window.CodeMirror.version);
+			return;
+		}
 
-		// This creates an icon in the left ribbon.
-		let ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		let statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'emacs-scroll-up-command',
+			name: 'scroll-up-command',
+			hotkeys: [
+				{ modifiers: ["Ctrl"], key: "V"},
+			],
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				this.execCodeMirrorCommand("goPageDown");
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
+
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				let markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: 'emacs-scroll-down-command',
+			name: 'scroll-down-command',
+			hotkeys: [
+				{ modifiers: ["Alt"], key: "V"},
+			],
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				this.execCodeMirrorCommand("goPageUp");
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.addCommand({
+			id: 'emacs-kill-word',
+			name: 'kill-word',
+			hotkeys: [
+				{ modifiers: ["Alt"], key: "D"},
+			],
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				this.execCodeMirrorCommand("delWordAfter");
+			}
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addCommand({
+			id: 'emacs-forward-word',
+			name: 'forward-word',
+			hotkeys: [
+				{ modifiers: ["Alt"], key: "F"},
+			],
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				this.execCodeMirrorCommand("goWordRight");
+			}
+		});
+
+		this.addCommand({
+			id: 'emacs-backward-word',
+			name: 'backward-word',
+			hotkeys: [
+				{ modifiers: ["Alt"], key: "B"},
+			],
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				this.execCodeMirrorCommand("goWordLeft");
+			}
+		});
 	}
 
 	onunload() {
-
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	// execCodeMirrorCommand works around restrictions in the public API
+	// for editors by finding CodeMirror editors directly. It works
+	// similarly to Editor.exec() but without exec's restrictions on
+	// command name.
+	execCodeMirrorCommand(cmd: string) {
+		this.app.workspace.iterateCodeMirrors(cm => {
+			if (cm.hasFocus()) {
+				cm.execCommand(cmd);
+			}
+		});
 	}
 }
